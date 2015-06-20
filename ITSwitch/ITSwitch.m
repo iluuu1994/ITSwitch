5747a2faa6bb5c96704b8d48b381c7e278faa605
Fabian Jäger <fabian@feingeist.io>
2015-06-20 15:17:53 +0200

    - fixed bi-directional binding

--- ITSwitch/ITSwitch.m
+++ ITSwitch/ITSwitch.m
@@ -54,6 +54,8 @@ static CGFloat const kDisabledOpacity = 0.5f;
 @property (nonatomic, readonly, strong) CALayer *knobLayer;
 @property (nonatomic, readonly, strong) CALayer *knobInsideLayer;
 
+- (void)propagateValue:(id)value forBinding:(NSString*)binding;
+
 @end
 
 
@@ -342,6 +344,7 @@ static CGFloat const kDisabledOpacity = 0.5f;
 - (void)setChecked:(BOOL)checked {
     if (_checked != checked) {
 		_checked = checked;
+        [self propagateValue:@(checked) forBinding:@"checked"];
     }
     
     [self reloadLayer];
@@ -480,6 +483,53 @@ static CGFloat const kDisabledOpacity = 0.5f;
 	else {
 		[super accessibilityPerformAction:actionString];
 	}
+}
+
+#pragma mark -
+#pragma mark Bindings Extension
+
+- (void)propagateValue:(id)value forBinding:(NSString*)binding
+{
+    NSParameterAssert(binding != nil);
+    
+    // WARNING: bindingInfo contains NSNull, so it must be accounted for
+    NSDictionary* bindingInfo = [self infoForBinding:binding];
+    if(!bindingInfo)
+        return; //there is no binding
+    
+    // apply the value transformer, if one has been set
+    NSDictionary* bindingOptions = [bindingInfo objectForKey:NSOptionsKey];
+    if(bindingOptions){
+        NSValueTransformer* transformer = [bindingOptions valueForKey:NSValueTransformerBindingOption];
+        if(!transformer || (id)transformer == [NSNull null]){
+            NSString* transformerName = [bindingOptions valueForKey:NSValueTransformerNameBindingOption];
+            if(transformerName && (id)transformerName != [NSNull null]){
+                transformer = [NSValueTransformer valueTransformerForName:transformerName];
+            }
+        }
+        
+        if(transformer && (id)transformer != [NSNull null]){
+            if([[transformer class] allowsReverseTransformation]){
+                value = [transformer reverseTransformedValue:value];
+            } else {
+                NSLog(@"WARNING: binding \"%@\" has value transformer, but it doesn't allow reverse transformations in %s", binding, __PRETTY_FUNCTION__);
+            }
+        }
+    }
+    
+    id boundObject = [bindingInfo objectForKey:NSObservedObjectKey];
+    if(!boundObject || boundObject == [NSNull null]){
+        NSLog(@"ERROR: NSObservedObjectKey was nil for binding \"%@\" in %s", binding, __PRETTY_FUNCTION__);
+        return;
+    }
+    
+    NSString* boundKeyPath = [bindingInfo objectForKey:NSObservedKeyPathKey];
+    if(!boundKeyPath || (id)boundKeyPath == [NSNull null]){
+        NSLog(@"ERROR: NSObservedKeyPathKey was nil for binding \"%@\" in %s", binding, __PRETTY_FUNCTION__);
+        return;
+    }
+    
+    [boundObject setValue:value forKeyPath:boundKeyPath];
 }
 
 @end
